87a1efbcc3dffba2642aa619b9e2772aaf7b6b59
brksnr <berksener04@gmail.com>
2025-01-28 17:39:42 +0300

    StarDto, RegistrationRequest,LoginResponse, LoginRequest oluşturuldu.

--- src/main/java/com/example/demo/dto/LoginResponse.java
+++ src/main/java/com/example/demo/dto/LoginResponse.java
@@ -1,4 +1,32 @@
 package com.example.demo.dto;
 
 public class LoginResponse {
+
+    private String token;
+    private String username;
+    private String email;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/example/demo/dto/StarDto.java
+++ src/main/java/com/example/demo/dto/StarDto.java
@@ -1,4 +1,73 @@
 package com.example.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+
+
+@JsonInclude(JsonInclude.Include.ALWAYS)
 public class StarDto {
+    private long id;
+    private String fullName;
+    private String age;
+    private String whoIs;
+    private String starImg;
+    private List<FilmDto> films;
+
+    public StarDto(long id, String fullName, String age, String whoIs, List<FilmDto> films, String starImg) {
+        this.id = id;
+        this.fullName = fullName;
+        this.age = age;
+        this.whoIs = whoIs;
+        this.films = films;
+        this.starImg = starImg;
+    }
+
+    public List<FilmDto> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<FilmDto> films) {
+        this.films = films;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getWhoIs() {
+        return whoIs;
+    }
+
+    public void setWhoIs(String whoIs) {
+        this.whoIs = whoIs;
+    }
+
+    public String getStarImg() {
+        return starImg;
+    }
+
+    public void setStarImg(String starImg) {
+        this.starImg = starImg;
+    }
 }
