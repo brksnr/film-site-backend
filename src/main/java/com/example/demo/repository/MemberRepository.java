ea3c7cce7e8cf19657202c91a9cf2fcd401c651a
brksnr <berksener04@gmail.com>
2025-01-29 22:31:36 +0300

    favori filmler için repository ve service katmanları oluşturuldu.

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Films;
 import com.example.demo.memberEntity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
@@ -13,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     @Query("SELECT m FROM Member m WHERE m.username = :username")
     Optional<Member> findUserByUsername(String username);
+
+    @Query("SELECT m.favorites FROM Member m WHERE m.username = :username")
+    Optional<Set<Films>> findFavoritesByUsername(String username);
 }
--- src/main/java/com/example/demo/services/MemberService.java
+++ src/main/java/com/example/demo/services/MemberService.java
@@ -1,12 +1,18 @@
 package com.example.demo.services;
 
 
+import com.example.demo.entity.Films;
+import com.example.demo.memberEntity.Member;
 import com.example.demo.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class MemberService implements UserDetailsService {
@@ -28,4 +34,32 @@ public class MemberService implements UserDetailsService {
                     throw new UsernameNotFoundException("User not valid!");
                 });
     }
+
+
+    public void addFavoriteFilm(String username, Films film) {
+        Optional<Member> memberOpt = memberRepository.findUserByUsername(username);
+        if (memberOpt.isPresent()) {
+            Member member = memberOpt.get();
+            member.getFavorites().add(film);
+            memberRepository.save(member);
+        }
+    }
+
+
+    public void removeFavoriteFilm(String username, Films film) {
+        Optional<Member> memberOpt = memberRepository.findUserByUsername(username);
+        if (memberOpt.isPresent()) {
+            Member member = memberOpt.get();
+            member.getFavorites().remove(film);
+            memberRepository.save(member);
+        }
+    }
+
+
+    public Set<Films> getFavoriteFilms(String username) {
+        Optional<Member> memberOpt = memberRepository.findUserByUsername(username);
+        return memberOpt.map(Member::getFavorites).orElse(Collections.emptySet());
+    }
+
+
 }
